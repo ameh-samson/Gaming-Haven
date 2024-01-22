fd14342aa14bceb6e9bc281d2e9413cd0a1af27f
Samson Ameh <amehsam69@gmail.com>
2024-01-22 20:09:29 +0100

    add pictures and function

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+// VARIABLE DECLARATION
+
 const menuBtn = document.querySelector("#menu-btn");
 menu = document.querySelector(".nav ul");
 exitBtn = document.querySelector(".exit-btn");
+loadMoreBtn = document.querySelector(".load-more-btn");
+moreGames = document.querySelectorAll(".more-games");
+
+// NAVIGATION
 
 menuBtn.addEventListener("click", () => {
   menu.style.transform = "translateY(0)";
@@ -9,3 +15,16 @@ menuBtn.addEventListener("click", () => {
 exitBtn.addEventListener("click", () => {
   menu.style.transform = "translateY(-100%)";
 });
+
+// LOAD MORE FUNCTION
+
+loadMoreBtn.addEventListener("click", viewMoreGames);
+
+function viewMoreGames() {
+  loadMoreBtn.textContent =
+    loadMoreBtn.textContent === "Load More" ? "Load Less" : "Load More";
+
+  moreGames.forEach((e) => {
+    e.classList.toggle("load-more");
+  });
+}
